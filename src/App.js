b9d10273f5e5acb01ff6329adbf4146bcfef6a98
ylem76 <ylem76@thebreeze.co.kr>
2021-06-17 12:06:16 +0900

    2-1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({ fav }) {
+    // console.log(propsArgs);
+    // props를 함수의 첫번째 인자로 가져옴
+    // {fav : 'kimchi'}
+    // 콘솔을 찍어보면 오브젝트로 가져오는 것을 확인할 수 있다.
+
+    // propsArgs.fav = {fav}
+    // ES6로 오브젝트의 fav만 꺼내서 사용할 수 있는데, 인자 부분도 위와 같이 수정해주고.
+    // 실제 사용시에는 아래와 같이 쓸 수 있다.
+    return <h3>I love { fav }</h3>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        wow
+        <Food fav="kimchi" />
+        <Food fav="햄버거" />
+        <Food fav="치킨" />
+        <Food fav="먹을거" />
     </div>
+    // Food component에 prop'fav'에 값'kimchi'를 줌.
+    // prop value에는 불리언, 오브젝트, 어레이 모두 가능
   );
 }
 
